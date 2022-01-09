f04c557eea94a1902348d9ff98fa5991696667a1
ChristianNikodemus <christian.nikodemus@gmail.com>
2022-01-09 12:11:59 -0500

    Connected api and error response handling

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -8,9 +8,37 @@ import {
 import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+//Declaring the api url that will provide data for the client app
+const apiUrl = 'https://my-films-db.herokuapp.com/';
+
+const token = localStorage.getItem('token');
+
+const headers = {
+  headers: new HttpHeaders({
+    Authorization: 'Bearer ' + token,
+  }),
+};
+
 @Injectable({
   providedIn: 'root',
 })
 export class FetchApiDataService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  // Non-typed response extraction
+  private extractResponseData(res: Response): any {
+    const body = res;
+    return body || {};
+  }
+
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}, ` + `Error body is: ${error.error}`
+      );
+    }
+    return throwError('Something bad happened; please try again later.');
+  }
 }
