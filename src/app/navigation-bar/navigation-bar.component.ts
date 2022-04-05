f0d384bdce377d347d13f7899530f25507e3b09b
ChristianNikodemus <christian.nikodemus@gmail.com>
2022-04-05 09:30:00 -0400

    Added username to navigation bar

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService, User } from '../fetch-api-data.service';
 import { Router } from '@angular/router';
 
@@ -8,11 +8,30 @@ import { Router } from '@angular/router';
   styleUrls: ['./navigation-bar.component.scss'],
 })
 export class NavigationBarComponent implements OnInit {
+  username: any = localStorage.getItem('user');
+  token: any = localStorage.getItem('token');
+
+  user: User | null = null;
+
   constructor(
-    private router: Router // public data: { //   User: Array<User>; // }
+    public fetchApiData: FetchApiDataService,
+    private router: Router
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser(): void {
+    console.log(this.token, this.username);
+    if (this.token && this.username) {
+      this.fetchApiData
+        .getUser(this.username, this.token)
+        .subscribe((resp: any) => {
+          this.user = resp;
+        });
+    }
+  }
 
   goToProfile(): void {
     this.router.navigate(['profile']);
